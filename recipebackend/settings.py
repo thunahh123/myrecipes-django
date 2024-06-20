aa69950a45500cb597f6bca972ecf919793c3d32
Thu Nguyen <nguyenngocanhthu18011994@gmail.com>
2024-06-20 17:04:49 -0400

    removed bullets for styles

--- recipebackend/settings.py
+++ recipebackend/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f@lv4_-p)7&jal=+x57nedxky!_54*js^2t49n-0xjs3trjtm1'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f@lv4_-p)7&jal=+x57nedxky!_54*js^2t49n-0xjs3trjtm1')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG','') != 'False'
 
 ALLOWED_HOSTS = []
 
