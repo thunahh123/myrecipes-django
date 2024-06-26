cfc048ea6edf7c0c1151df6e4d92763975a69ef2
Thu Nguyen <nguyenngocanhthu18011994@gmail.com>
2024-06-26 17:00:20 -0400

    updated ingredient model

--- recipes/models.py
+++ recipes/models.py
@@ -4,13 +4,6 @@ from users.models import CustomUser
 
 # Create your models here.
 
-class Ingredient(models.Model):
-    name = models.CharField(max_length=40)
-
-    def __str__(self):
-        return f"{self.name}"
-    
-
 class Recipe(models.Model):
     name = models.CharField(max_length=64)
     time = models.IntegerField()
@@ -19,10 +12,15 @@ class Recipe(models.Model):
     direction = models.TextField()
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="recipes", null=True)
     date_created = models.DateTimeField(auto_now_add=True)
-
-    # def get_directions_as_list(self):
-    #     return self.directions.split('\n')
-
+    date_updated = models.DateTimeField(auto_now=True)
+    is_featured = models.BooleanField(default=False)
     def __str__(self) :
         return f":{self.name}: {self.description} (Created by: {self.author}) "
     
+class Ingredient(models.Model):
+    name = models.CharField(max_length=40)
+    recipes =  models.ManyToManyField(Recipe, blank=True, related_name="ingredients")
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.name}"
