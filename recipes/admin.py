89be2cb10079304bb0db8f0e7269a7db334a2aff
thunahh123 <88802875+thunahh123@users.noreply.github.com>
2024-07-04 17:35:29 -0400

    Added comment model

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient
+from .models import Recipe, Ingredient, Unit, Comment
 
 # Register your models here.
 admin.site.register(Recipe)
 
 admin.site.register(Ingredient)
+admin.site.register(Unit)
+admin.site.register(Comment)
+
+
 
--- recipes/models.py
+++ recipes/models.py
@@ -33,3 +33,22 @@ class IngredientRecipe(models.Model):
     # def __str__(self):
     #     return f"{self.name}"
 
+class Unit(models.Model):
+    unit = models.CharField(max_length=40)
+    abbr = models.CharField(max_length=25, null=True)
+    plural = models.CharField(max_length=40)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+         return f"{self.unit}"
+
+class Comment(models.Model):
+    parent_id = models.ForeignKey('self', on_delete=models.CASCADE, related_name="replies", null=True, blank=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="comments", null=True)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="comments")
+    comment = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.recipe}'
